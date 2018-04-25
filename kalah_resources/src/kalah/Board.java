0d7e78999402428e9a1328be23122ac09ee4312e
JDamms <j.damms@hotmail.com>
2018-04-25 21:58:12 +1200

    working... need to do tests

---
+++ kalah_resources/src/kalah/Board.java
@@ -0,0 +1,144 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalah;
+
+/**
+ *
+ * @author jdamm
+ */
+public class Board {
+    int[] values = new int[14]; // 0 is p1 store value, 1 is p2 store value //  2-7 is p1 house values, 8-13 is p2 house values
+    House[] player1Houses = new House[6];
+    House[] player2Houses = new House[6];
+    Store player1Store = null;
+    Store player2Store = null;
+    
+    public Board(House[] player1House, House[] player2House, Store p1Store, Store p2Store){
+        for(int i = 0; i < 6; i++){
+            this.player1Houses[i] = player1House[i];
+            this.player2Houses[i] = player2House[i];
+        }
+        this.player1Store = p1Store;
+        this.player2Store = p2Store;
+    }
+    
+    public int[] updateBoard(){
+        //
+        values[0] = this.player1Store.getSeeds(); // player 1 store
+        values[1] = this.player2Store.getSeeds(); // player 2 store
+        for(int i = 0; i < 6; i++){
+            values[2+i] = this.player1Houses[i].getSeeds(); // player 1 houses
+            values[8+i] = this.player2Houses[i].getSeeds(); // player 2 houses
+        }
+        return values;
+    }
+    
+    public boolean checkGameStatus(){ // check if house has been cleared
+        //
+        int count1 = 0, count2 = 0;
+        for(int i = 0; i < 6; i++){
+            if(this.player1Houses[i].getSeeds() == 0){
+                count1++;
+            }
+            if(this.player2Houses[i].getSeeds() == 0){
+                count2++;
+            }
+            if(count1 == 6 || count2 == 6){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean moveSeeds(int houseNum, boolean playerTurn){ // check if house has been cleared
+        // test
+        int seedsInHouse = 0;
+        int house = houseNum;
+        boolean turn = playerTurn;
+        if(playerTurn){
+            seedsInHouse = this.player2Houses[house].getSeeds();
+            this.player2Houses[house].setSeeds(0);
+        } else {
+            seedsInHouse = this.player1Houses[house].getSeeds();
+            this.player1Houses[house].setSeeds(0);
+        }     
+        
+        while(seedsInHouse > 0){
+            house++; // next house
+            if(house == 6){ // add seed into store if it's your turn
+                
+                house = -1;
+                if(turn == playerTurn){ // add to players store
+                    if(playerTurn){ // player 2 store
+                        this.player2Store.incrementSeeds();
+                    } else { // player 1 store
+                        this.player1Store.incrementSeeds();
+                    }
+                    seedsInHouse--; // used a seed
+                    if(seedsInHouse == 0){
+                        house = 0;
+                    }
+                }
+                turn = !turn;
+              
+            } 
+            else { // add to houses
+                
+                if(turn){ // player 2
+                    this.player2Houses[house].incrementSeeds();
+                } else { // player 1
+                    this.player1Houses[house].incrementSeeds();
+                }
+                seedsInHouse--; // used a seed
+            }            
+        }
+        
+        // check if current house had 0 seeds if so add pebble and opposite to player store
+        return checkEndingHouse(turn, playerTurn, house);
+    }
+    
+    public boolean checkEndingHouse(boolean turn, boolean playerTurn, int house){ // check if house has been cleared
+        
+        if(turn){ // ends on player 2 side
+            System.out.println("Ended on player 2 side checks "+house);
+            if(this.player2Houses[house].getSeeds() == 1){
+                this.player2Houses[house].setSeeds(0);
+                int seedsToAdd = 0;
+                   
+                seedsToAdd = 1 + this.player1Houses[0+(5-house)].getSeeds(); // seeds plus opposite to add to score
+                this.player1Houses[0+(5-house)].setSeeds(0); // sets opposite house to 0 seeds
+                 
+                if(playerTurn){ // player 2 turn
+                    this.player2Store.add(seedsToAdd);
+                } else { // player 1 turn
+                    this.player1Store.add(seedsToAdd);
+                }
+                
+                return true;
+            }
+        } 
+        else { // ends on player 1 side
+            System.out.println("Ended on player 1 side checks "+house);
+            if(this.player1Houses[house].getSeeds() == 1){
+                this.player1Houses[house].setSeeds(0);
+                int seedsToAdd = 0;
+                
+                seedsToAdd = 1 + this.player2Houses[0+(5-house)].getSeeds(); // seeds plus opposite to add to score
+                this.player2Houses[0+(5-house)].setSeeds(0); // sets opposite house to 0 seeds
+                         
+                if(playerTurn){ // player 2 turn
+                    this.player2Store.add(seedsToAdd);
+                } else { // player 1 turn
+                    this.player1Store.add(seedsToAdd);
+                }
+                
+                return true;
+            }
+        }
+        return false;
+    }
+  
+} // end of board
--- kalah_resources/src/kalah/Kalah.java
+++ kalah_resources/src/kalah/Kalah.java
@@ -2,25 +2,91 @@ package kalah;
 
 import com.qualitascorpus.testsupport.IO;
 import com.qualitascorpus.testsupport.MockIO;
+import java.lang.String;
 
 /**
  * This class is the starting point for a Kalah implementation using
  * the test infrastructure.
  */
 public class Kalah {
+    boolean playerTurn = false; // false = player1, true = player2
+    boolean gameOver = false;
+    boolean goAgain = false;
+    
 	public static void main(String[] args) {
+                /*Animal chick1 = new Chicken(1, "M", 7);
+                //chick1.move();
+                
+                Animal sparrow1 = new Sparrow(2, "M", 7); 
+                //sparrow1.move();
+                
+                Animal fish1 = new Fish(2, "M", 7); 
+                //fish1.move();
+                
+                moveAnimal(chick1);
+                moveAnimal(sparrow1);
+                moveAnimal(fish1);*/
+               
+                
+                
 		new Kalah().play(new MockIO());
-                while(true){
-                    // keep program alive
-                }
 	}
+        
+        /*public static void moveAnimal(Animal animal){ // allows for more dynamic function
+            animal.move();
+        }*/
+        
 	public void play(IO io) {
 		// Replace what's below with your implementation
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-		io.println("| P2 | 6[ 4] | 5[ 4] | 4[ 4] | 3[ 4] | 2[ 4] | 1[ 4] |  0 |");
-		io.println("|    |-------+-------+-------+-------+-------+-------|    |");
-		io.println("|  0 | 1[ 4] | 2[ 4] | 3[ 4] | 4[ 4] | 5[ 4] | 6[ 4] | P1 |");
-		io.println("+----+-------+-------+-------+-------+-------+-------+----+");
-		io.println("Player 1's turn - Specify house number or 'q' to quit: ");
+                // Setup the board
+                House[] player1Houses = new House[6];
+                House[] player2Houses = new House[6];
+                for(int i = 0; i < 6; i++){
+                    player1Houses[i] = new House(4); // 4 seeds per house
+                    player2Houses[i] = new House(4); // 4 seeds per house
+                }
+                Store player1 = new Store(0);
+                Store player2 = new Store(0);
+                
+                Board game = new Board(player1Houses, player2Houses, player1, player2);
+                
+                int[] values = new int[14];
+                while(true){
+                        // keep program alive3
+                    String inputText = "Specify house number ";
+                    int selectedHouse = io.readInteger(inputText, 1, 6, 0, inputText) - 1;
+                    io.println("Selected House: " + selectedHouse);
+                    
+                    goAgain = game.moveSeeds(selectedHouse, playerTurn);
+                    values = game.updateBoard();
+                    gameOver = game.checkGameStatus(); // check for game status
+                    
+                    if(!goAgain){ // if need to change player
+                        playerTurn = !playerTurn;
+                    }
+                    
+                    if(gameOver){
+                        io.println(" ");
+                        io.println("-------------------------------");
+                        io.println("GAME OVER");
+                        io.println("Player 1 Score: " + values[0]);
+                        io.println("Player 2 Score: " + values[1]);
+                        io.println("-------------------------------");
+                    } else {
+                        io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+                        io.println("| P2 | 6[ "+values[13]+"] | 5[ "+values[12]+"] | 4[ "+values[11]+"] | 3[ "+values[10]+"] | 2[ "+values[9]+"] | 1[ "+values[8]+"] |  "+values[0]+" |");
+                        io.println("|    |-------+-------+-------+-------+-------+-------|    |");
+                        io.println("|  "+values[1]+" | 1[ "+values[2]+"] | 2[ "+values[3]+"] | 3[ "+values[4]+"] | 4[ "+values[5]+"] | 5[ "+values[6]+"] | 6[ "+values[7]+"] | P1 |");
+                        io.println("+----+-------+-------+-------+-------+-------+-------+----+");
+                        if(playerTurn){
+                            io.println("Player 2's turn - Specify house number or 'q' to quit: ");
+                        } else {
+                            io.println("Player 1's turn - Specify house number or 'q' to quit: ");
+                        }
+                    }
+                    
+                }
 	}
+        
+
 }
